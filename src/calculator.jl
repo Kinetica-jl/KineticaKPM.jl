038399414f91e08fe309d6bb31ca2cda38bab5c3
joegilkes <j.gilkes@warwick.ac.uk>
2023-09-26 09:54:16 +0100

    Documentation pass for KPMBasicCalculator

--- src/calculator.jl
+++ src/calculator.jl
@@ -1,4 +1,24 @@
 """
+Basic KPM kinetic calculator for reactions.
+
+Kinetic calculator that uses KPM to predict activation
+energies for reactions and uses these predictions within
+the Arrhenius equation to calculate rate constants.
+
+Basic calculator uses a flat `RT/h` Arrhenius prefactor
+for all reactions. This is usually not accurate enough
+to enable sensible kinetic simulations.
+
+Implemented conditions:
+* Temperature (`T`, unit: K)
+
+Requires:
+* Reaction energies (`rd.dH`, unit: eV)
+* Trained KPM model (`kpm.model_path`, loaded when `KPMRun` is instantiated)
+
+Has support for dispatching with/without a maximum rate constant
+`k_max` and scaling by time unit `t_unit` (assuming rates are
+provided in units of /s).
 """
 struct KPMBasicCalculator{kmType, uType, tType} <: KineticaCore.AbstractKineticCalculator
     Ea::Vector{uType}
@@ -8,6 +28,9 @@ struct KPMBasicCalculator{kmType, uType, tType} <: KineticaCore.AbstractKineticC
 end
 
 """
+    calculator = KPMBasicCalculator(rd, sd, kpm[, k_max, t_unit])
+
+Outer constructor method for basic KPM calculator.
 """
 function KPMBasicCalculator(rd::RxData, sd::SpeciesData, kpm::KPMRun;
         k_max::Union{Nothing, uType}=nothing, 
@@ -17,6 +40,17 @@ function KPMBasicCalculator(rd::RxData, sd::SpeciesData, kpm::KPMRun;
     return KPMBasicCalculator(Ea, k_max, t_unit, t_mult)
 end
 
+"""
+    rates = calculator(; T)
+
+Calculate rates with basic KPM calculator.
+
+Requires temperature (`T`) as a keyword argument.
+
+Automatically dispatches to a method with correct formula for
+`k_max`-aware calculation if this is defined in the underlying
+`KPMBasicCalculator`.
+"""
 function (calc::KPMBasicCalculator{uType, uType, tType})(; T::Number) where {uType, tType}
     R = 8.314462618 # Gas constant (J/K/mol)
     h = 6.626070e-34 # Planck constant (Js)
