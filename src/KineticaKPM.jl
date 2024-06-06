e67695e7e0f6b3c43fd275b028df79727f5e72c5
joegilkes <j.gilkes@warwick.ac.uk>
2024-06-06 16:55:05 +0100

    Removed internal version number

--- src/KineticaKPM.jl
+++ src/KineticaKPM.jl
@@ -15,7 +15,6 @@ using Measurements
 using DelimitedFiles
 using Statistics
 
-const version = VersionNumber(0, 4, 1)
 const rdChem = PythonCall.pynew()
 const kpm_utils = PythonCall.pynew()
 const pysys = PythonCall.pynew()
